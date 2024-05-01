84407d9056a81f651742e6a0a76566f9566d2b1c
iago silva <iago.developer01@gmail.com>
2024-05-01 18:55:37 -0300

    add: metodo para descriptografar jwt

---
+++ server/src/main/java/com/petx/service/UserTokenService.java
@@ -0,0 +1,32 @@
+package com.petx.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserTokenService {
+
+    @Value("${jwt.chave-assinatura}")
+    private String chaveAssinatura;
+
+    public Long getIdDoUsuarioDoTokenJWT(String token) {
+        try{
+            if (token.startsWith("Bearer ")) {
+                token = token.substring(7);
+            }
+
+            Claims claims = Jwts.parser()
+                    .setSigningKey(chaveAssinatura)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            Long userId = Long.valueOf(claims.get("userid").toString());
+            return userId;
+        }catch (Exception e) {
+            return null;
+        }
+    }
+}
