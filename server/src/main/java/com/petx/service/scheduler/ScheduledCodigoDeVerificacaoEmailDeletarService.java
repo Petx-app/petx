6cc79814e820eea6a7c0cb4a1f3c9d5a0da64bdc
iago silva <iago.developer01@gmail.com>
2024-05-21 11:52:28 -0300

    add: metodo de remocao de codigos expirados de troca de senha

--- server/src/main/java/com/petx/service/scheduler/ScheduledCodigoDeVerificacaoEmailDeletarService.java
+++ server/src/main/java/com/petx/service/scheduler/ScheduledCodigoDeVerificacaoEmailDeletarService.java
@@ -1,13 +1,15 @@
 package com.petx.service.scheduler;
 
 import com.petx.domain.usuario.ValidacaoEmail;
+import com.petx.domain.usuario.ValidacaoSenha;
+import com.petx.repository.ValidacaoSenhaRepository;
 import com.petx.repository.ValidacaoUsuarioRepository;
-import com.petx.utils.GerarHoraAtual;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
-import static com.petx.utils.GerarHoraAtual.horaAtual;
 
 @Service
 public class ScheduledCodigoDeVerificacaoEmailDeletarService {
@@ -16,16 +18,29 @@ public class ScheduledCodigoDeVerificacaoEmailDeletarService {
     private ValidacaoUsuarioRepository validacaoUsuarioRepository;
 
     @Autowired
-    private GerarHoraAtual gerarHoraAtual;
+    private ValidacaoSenhaRepository validacaoSenhaRepository;
 
     @Scheduled(cron = "0 0 11 * * *")
-    public void tempoExpiracao(){
+    public void tempoExpiracaoEmail(){
         List<ValidacaoEmail> registroCodigoVerificacao = validacaoUsuarioRepository.findAll();
-        System.out.println("rodando shcundele");
         if(!registroCodigoVerificacao.isEmpty()){
             for(ValidacaoEmail registro : registroCodigoVerificacao){
-                if((horaAtual() - registro.getHoraInserida()) > 2 ){
+                if(registro.getHoraInserida().toLocalDate().isEqual(LocalDateTime.now().toLocalDate())
+                        && ChronoUnit.MINUTES.between(registro.getHoraInserida(), LocalDateTime.now()) > 2) {
                     validacaoUsuarioRepository.deleteById(registro.getId());
+                }
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 0 11 * * *")
+    public void tempoExpiracaoSenha(){
+        List<ValidacaoSenha> registroCodigoVerificacao = validacaoSenhaRepository.findAll();
+        if(!registroCodigoVerificacao.isEmpty()){
+            for(ValidacaoSenha registro : registroCodigoVerificacao){
+                if(registro.getHoraInserida().toLocalDate().isEqual(LocalDateTime.now().toLocalDate())
+                        && ChronoUnit.MINUTES.between(registro.getHoraInserida(), LocalDateTime.now()) > 5) {
+                    validacaoSenhaRepository.deleteById(registro.getId());
                 }
             }
         }
